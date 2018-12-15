c6cae004d28270bfc31e14269ac9a2a5359ca470
Xybolx <xybolx@gmail.com>
2018-12-14 23:37:55 -0700

    "spotify sdk added!"

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,17 +5,49 @@
 //   "spy hunter", "metal gear", "metroid", "double dribble", "baseball stars",
 //   "bionic commando", "tetris", "double dragon", "kung fu", "paperboy"//
 
-$(document).ready(function () {
+
   // document.getElementById("nesPic").style.cssText = "display: none";
   
   
+  
+  window.onSpotifyWebPlaybackSDKReady = () => {
+   const token = 'BQCbDFNQMvFxns8cyL75XlHWFNb8V-PoKRa7KE17CXNUNP7NlwaHEAPfytGBYE42avLhA4Rr4x7Xa5O7iYAJ1RxNnDwZTQPxNOWCZFGPMs_Tc1THFWy9itI732P3AAPQdUiXXaV3VJuGfmgkC774Qngw8haHLv0';
+   const player = new Spotify.Player({
+     name: 'Web Playback SDK Quick Start Player',
+     getOAuthToken: cb => { cb(token); }
+   });
+   
+   
+   // Error handling
+   player.addListener('initialization_error', ({ message }) => { console.error(message); });
+   player.addListener('authentication_error', ({ message }) => { console.error(message); });
+   player.addListener('account_error', ({ message }) => { console.error(message); });
+   player.addListener('playback_error', ({ message }) => { console.error(message); });
+   
+   // Playback status updates
+   player.addListener('player_state_changed', state => { console.log(state); });
+   
+   // Ready
+   player.addListener('ready', ({ device_id }) => {
+     console.log('Ready with Device ID', device_id);
+   });
+   
+   // Not Ready
+   player.addListener('not_ready', ({ device_id }) => {
+     console.log('Device ID has gone offline', device_id);
+   });
+   
+   // Connect to the player!
+   player.connect();
+
+   $(document).ready(function() {
   
   var games = [
       "super mario bros.", "mike tyson's punch out", "kid icarus", "excitebike", "super mario bros. 2", "the legend of zelda", "mega man", "duck hunt", "double dragon", "spy hunter", "tecmo bowl", "contra"
       
     
 
-  ];
+    ];
   // function to make buttons and add to page
   function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
     $(areaToAddTo).empty();
@@ -33,7 +65,7 @@ $(document).ready(function () {
 
   
   
-    
+  
     
     $(document).on("click", ".game-button", function () {
     $("#games").empty();
@@ -46,44 +78,44 @@ $(document).ready(function () {
 
     var type = $(this).attr("data-type");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=fuh24fesVB79bhfW3zSSXJZCZhnTqxHP&limit=10";
-
+    
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-      .then(function (response) {
-        var results = response.data;
+    .then(function (response) {
+      var results = response.data;
+      
+      for (var i = 0; i < results.length; i++) {
+        var gameDiv = $("<div class='game-item'>");
 
-        for (var i = 0; i < results.length; i++) {
-          var gameDiv = $("<div class='game-item'>");
+        var rating = results[i].rating;
+        
+        var p = $("<p>").text("Rating: " + rating);
 
-          var rating = results[i].rating;
-
-          var p = $("<p>").text("Rating: " + rating);
-
-          var animated = results[i].images.fixed_height.url;
-          var still = results[i].images.fixed_height_still.url;
-
-          var gameImage = $("<img>");
-          gameImage.attr("src", still);
-          gameImage.attr("data-still", still);
-          gameImage.attr("data-animate", animated);
-          gameImage.attr("data-state", "still");
-          gameImage.addClass("game-image");
-
-          gameDiv.append(p);
-          gameDiv.append(gameImage);
-
-          $("#games").append(gameDiv);
-        }
-      });
+        var animated = results[i].images.fixed_height.url;
+        var still = results[i].images.fixed_height_still.url;
+        
+        var gameImage = $("<img>");
+        gameImage.attr("src", still);
+        gameImage.attr("data-still", still);
+        gameImage.attr("data-animate", animated);
+        gameImage.attr("data-state", "still");
+        gameImage.addClass("game-image");
+        
+        gameDiv.append(p);
+        gameDiv.append(gameImage);
+        
+        $("#games").append(gameDiv);
+      }
+    });
   });
-
+  
   $(document).on("click", ".game-image", function () {
     $("#toggleGif")[0].play();
-
+    
     var state = $(this).attr("data-state");
-
+    
     if (state === "still") {
       $(this).attr("src", $(this).attr("data-animate"));
       $(this).attr("data-state", "animate");
@@ -91,45 +123,47 @@ $(document).ready(function () {
     else {
       $(this).attr("src", $(this).attr("data-still"));
       $(this).attr("data-state", "still");
-    }
-  });
-
-  $("#add-game").on("click", function (event) {
-    event.preventDefault();
-    $("#addGif")[0].play();
-    var newGame = $("input").eq(0).val();
-    
-    
-
-   if (newGame.length > 2) {
-      games.push(newGame);
-      console.log(games);
-      localStorage.clear();
-      localStorage.setItem("newGames", JSON.stringify(games));
-      localStorage.getItem("newGames");
+   }
+    });
+    $("#add-game").on("click", function (event) {
+          event.preventDefault();
+          $("#addGif")[0].play();
+          var newGame = $("input").eq(0).val();
       
-       
-    } 
-
+      
+      
+      
+      if (newGame.length > 2) {
+        games.push(newGame);
+        console.log(games);
+        localStorage.clear();
+        localStorage.setItem("newGames", JSON.stringify(games));
+        localStorage.getItem("newGames");
+      }
+        
+        
     
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      populateButtons(games, "game-button", "#game-buttons");
+  });
     
-    
+      
+      
     
     populateButtons(games, "game-button", "#game-buttons");
-    
   });
-  
-  
-  
-  
-  
-  
-   populateButtons(games, "game-button", "#game-buttons");
- 
-  
-  
-});
+    
+}
+    
 
-
-
-
+  
